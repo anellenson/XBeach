933263843fbb573b1b18ecff2c447440aa1298e9
Ashley Ellenson <anellenson@gmail.com>
2018-10-17 13:25:16 -0700

    generated least squares fit to find the best paramete

--- python/leastsquaresfitbathy.py
+++ python/leastsquaresfitbathy.py
@@ -1,3 +1,4 @@
+from lmfit import minimize, Parameters
 import numpy as np
 import pandas as pd
 import datetime as dt
@@ -9,10 +10,13 @@ interactive(True)
 from scipy.optimize import fsolve
 import bathytools as bt
 
-bathyset = bt.retrieveData(2017,3,1)
-pNumber = 960
-time,x,transect = bt.transect(bathyset,pNumber)
-pl.plot(x,transect)
+def genbathy(pars, x, transect):
+    parvals = pars.valuesdict()
+    xb = parvals['xb']
+    hsea = parvals['hsea']
+    Sval_coeff = parvals['Sval_coeff']
+    x_ = parvals['x_']
+
 
 
     ##beta is the asymptotic offshore slope, assume an x number to be seaward of the active
@@ -36,9 +40,8 @@ pl.plot(x,transect)
 
     ##Choose a point to find depth h_, randomly chose 60 offshore of the offshore point
     #Criteria is that betaOffshore * x Offshore < hOffshore
-    h_ = transect[offshore+offset]
-    x_ = x[offshore+offset]
 
+    h_ = np.interp(x_,x,transect)
 
     #Now, find gamma and k by solving the two functions (equations 5 and 7) simultaneously
     def equations5and7(vars):
@@ -50,7 +53,7 @@ pl.plot(x,transect)
     sol = fsolve(equations5and7,guess)
     gamma = np.float(sol[0])
     k = np.float(sol[1])
-    print(gamma,k)
+
 
     #Find background transect shape, h0
     h0 = [gamma*(np.exp(-k*xx) -1) + beta_0*xx for xx in x]
@@ -68,7 +71,8 @@ pl.plot(x,transect)
     h0_shore = h0[0]
     h0_sea = h0[offshore]
     x_sea = x[offshore]
-    Smax = 0.2*h0_sea
+
+    Smax = Sval_coeff*h0_sea
     bar_zone_depth = [(hh - h0_shore)/(h0_sea - h0_shore) for hh in h0]
     expfun = [np.exp(-((1 - ((hh-h0_shore)/(h0_sea-h0_shore)))**a - b)**2/c) for hh in h0]
     maxInd = np.where(expfun == np.nanmax(expfun))[0][0]
@@ -104,5 +108,40 @@ pl.plot(x,transect)
     hbar = -1*np.multiply(S,cos)
     transect_simulated = h0 + hbar
 
+    return transect, transect_simulated
+
+def residuals(params,x,transect):
+    transect, transect_simulated = genbathy(params,x,transect)
     residuals = transect - transect_simulated
     return residuals
+
+#bathyset = bt.retrieveData(2017,3,1)
+pNumber = 960
+time,x,transect = bt.transect(bathyset,pNumber)
+
+
+params = Parameters()
+params.add('xb',value = 159.4)
+params.add('hsea',value = 3.9, min = 3, max = 4.5)
+params.add('Sval_coeff',value = 0.2)
+params.add('x_',value = 400)
+out = minimize(residuals, params, args = (x,transect))
+transect,transect_simulated = genbathy(out.params,x,transect)
+
+##Now add a shoreline to the transect
+x = np.linspace(1,100)
+shore_range = np.where((transect < 0.5) & (transect > -0.5))[0]
+beta_s = np.divide(np.diff(transect[shore_range]),np.diff(x[shore_range]))
+beta_s = np.mean(beta_s)
+
+#Find how long the x vector has to be for it to reach 0 from 4 meters
+x_len = fsolve(lambda b0: 4 + beta_s*b0 - transect_simulated[0], 10)
+x_len = np.ceil(x_len)[0]
+x_shoreline = np.linspace(0,x_len,num = x_len)
+shore_slope = np.multiply(beta_s,x_shoreline)+4
+transect_simulated[0] = shore_slope[-1]
+transect_simulated = np.append(shore_slope,-1*transect_simulated)
+diff_x = x_shoreline[-1]-x[shore_ind]
+x = x[shore_ind:] + diff_x
+
+x = np.append(x_shoreline, x)
